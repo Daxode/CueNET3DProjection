628c2bce892b02313549aff80e33a8ec7014ee0c
Daniel Kierkegaard Andersen <dani485b@gmail.com>
2018-12-11 13:21:00 +0100

    Tilfoejede nogle klasser

---
+++ SRP 3D Projection on Keyboard/Matrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP_3D_Projection_on_Keyboard {
+    class Matrix {
+
+        //Matrix multiplikation
+        public static void Multiply(float[][] a, float[][] b) {
+
+        }
+    }
+}
